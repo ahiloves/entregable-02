972bf0e8e876fbc877c6ea4c694f4a524ba97c82
Abraham Alberto Queccano Gallegos <abrahamalbertoqueccanogallegos@MacBook-Pro.local>
2024-01-27 12:03:37 -0500

    advance

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -7,6 +7,49 @@ const getAll = catchError(async(req, res) => {
     return res.json( user /* valor a retornar */)
 });
 
+const create = catchError(async (req, res)=>{
+    const { firstName, lastName, email, password, birthday } = req.body
+    const newBody = { firstName, lastName, email, password, birthday }
+    const user = await User.create(newBody)
+    return res.json(user)
+})
+
+const getOne = catchError(async (req, res)=>{
+    const { id } = req.params
+    const user = await User.findByPk(id)
+    if(!user) return res.sendStatus(404)
+    return res.json(user)
+})
+
+const destroy = catchError(async (req, res) =>{
+    const { id } = req.params
+    const user = await User.findByPk(id)
+    if (!user) return res.sendStatus(404)
+
+    await User.destroy({where:{ id }})
+    // await song.destroy()
+    return res.send('User Deleted').status(204)
+})
+
+const update = catchError(async (req, res)=>{
+    const { id } = req.params
+    const { firstName, lastName,  email, password,  birthday } = req.body
+    const newBody = { firstName, lastName,  email, password,  birthday }
+
+    const user = await User.findByPk(id)
+    if (!user) return res.sendStatus(404)
+
+    const userUpdate = await user.update(
+        newBody,
+        { where: { id }, returning: true}
+    )
+    return res.send(userUpdate[1][0])
+})
+
 module.exports = {
-    getAll
+    getAll,
+    create,
+    getOne,
+    destroy,
+    update
 }
